84ab0ad7054ba180e693d435fe5f9d84e0d24de1
ramotheo <u24666981@tuks.co.za>
2026-09-07 22:34:34 +0200

    hj

---
+++ client/src/pages/routes.tsx
@@ -0,0 +1,7 @@
+const DEFAULT_API_URL = "https://forgehttp.onrender.com"
+
+export const API_URL = (
+    import.meta.env.VITE_API_URL || DEFAULT_API_URL
+).replace(/\/$/, "");
+
+// CRUD: GET, POST, PUT, DELETE
